e78f998497c20e94460b7d100943572ac52904b1
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-04 14:32:34 +0100

    binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,10 +7,11 @@
  *
  * Return: Pointer to the new node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -18,7 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	
+
 	return (new_node);
 }
 
