cf366ef6aaaf6cdeecb15c5cf973aabc8b5b8928
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-05 12:38:36 +0100

    is root?

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to the node to check
+ *
+ * Return: 1 on success otherwise 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0)
+	}
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
