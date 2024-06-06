ccbbadbb331acbf72b2c8121d14fc138642daa4e
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-06 18:31:59 +0100

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
-	return (0);
+	else
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		binary_tree_is_full(tree->left);
+		binary_tree_is_full(tree->right);
+	}
 }
