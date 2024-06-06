b30c8cac0c1ae5bd00eb118badfd7afafec4ad8c
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-06 16:35:19 +0100

    node count

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,11 +9,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t node_count = 0;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL || tree->right != NULL)
-		return (1);
-	binary_tree_nodes(tree->left);
-	binary_tree_nodes(tree->right);
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+                node_count = 1;
+	node_count += binary_tree_nodes(tree->left);
+	node_count += binary_tree_nodes(tree->right);
+	return (node_count);
 }
