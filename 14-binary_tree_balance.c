a8a13388260e5d548ec37cfa98963e81382510c7
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-06 17:34:24 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,10 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		count_left = 1 + binary_tree_height(tree->left);
-	if (tree->right != NULL)
-		count_right = 1 + binary_tree_height(tree->right);
+	count_left = 1 + binary_tree_height(tree->left);
+	count_right = 1 + binary_tree_height(tree->right);
 	return (count_left > count_right ? count_left : count_right);
 }
 /**
@@ -32,5 +30,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int l_height = binary_tree_height(tree->left);
 	int r_height = binary_tree_height(tree->right);
 
+	if (tree == NULL)
+		return (0);
 	return (l_height - r_height);
 }
